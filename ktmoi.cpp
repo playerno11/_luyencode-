df8d06f5a965f2fe82be2a34bde7bd831291d05c
playerno11 <130674643+playerno11@users.noreply.github.com>
2023-05-28 15:11:45 +0700

    Add files via upload

---
+++ ktmoi.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+char s ;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	//freopen("ktmoi.inp","r",stdin);
+	//freopen("ktmoi.out","w",stdout);
+	cin >> s;
+	cout << (char)(int(s) + 32) ;
+}
